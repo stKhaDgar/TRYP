50dc66118aa139c8a845cca0e579791b6c60365d
stMakTavish <stmaktavish@gmail.com>
2019-03-14 17:01:07 +0200

    new card logic update / scanner added

--- app/src/main/java/com/rdev/tryp/payment/AddCardFragment.kt
+++ app/src/main/java/com/rdev/tryp/payment/AddCardFragment.kt
@@ -12,17 +12,42 @@ import kotlinx.android.synthetic.main.fragment_add_card.view.*
 
 class AddCardFragment : Fragment() {
 
+    private var currentTypeCard = ""
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_add_card, container, false)
 
         onClickListener(view)
+        initCardForm(view)
 
         return view
     }
 
-    private fun onClickListener(view: View){
-        view.back_btn.setOnClickListener { (activity as? ContentActivity)?.startFragment(ContentActivity.TYPE_PAYMENT) }
+    private fun initCardForm(view: View){
+        view.cardForm.cardRequired(true)
+                .expirationRequired(true)
+                .cvvRequired(true)
+                .actionLabel("Purchase")
+                .setup(activity as ContentActivity)
+
+        view.cardForm.setOnCardTypeChangedListener {
+            currentTypeCard = it.name
+        }
+
+        if(view.cardForm.isCardScanningAvailable) { view.btnCamera.visibility = View.VISIBLE }
+
+        view.btnCamera.setOnClickListener { view.cardForm.scanCard(activity as ContentActivity) }
     }
 
+    private fun onClickListener(view: View){
+        view.back_btn.setOnClickListener { (activity as? ContentActivity)?.startFragment(ContentActivity.TYPE_PAYMENT) }
+        view.btnAddNewCard.setOnClickListener {
+            if(!view.cardForm.isValid){
+                view.cardForm.validate()
+            } else {
+
+            }
+        }
+    }
 }
