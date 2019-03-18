70ea551fad74f0b5475a03893d7221e14e355f13
stMakTavish <stmaktavish@gmail.com>
2019-03-18 20:57:12 +0200

    withdrawRewards screen complete

--- app/src/main/java/com/rdev/tryp/blocks/reward_profile/RewardWithdrawFragment.kt
+++ app/src/main/java/com/rdev/tryp/blocks/reward_profile/RewardWithdrawFragment.kt
@@ -5,14 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.rdev.tryp.ContentActivity
 import com.rdev.tryp.R
+import kotlinx.android.synthetic.main.fragment_reward_withdraw.view.*
 
-class RewardWithdrawFragment : Fragment() {
+class RewardWithdrawFragment : Fragment(), View.OnClickListener {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_reward_withdraw, container, false)
+        view.confirmedText.text = view.context.getText(R.string.rewards_withdraw_confirmed_text)
+        view.back_btn.setOnClickListener(this)
         return view
     }
 
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.back_btn -> { (activity as? ContentActivity)?.startFragment(ContentActivity.TYPE_REWARD_POINTS) }
+        }
+    }
 }
